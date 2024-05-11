fe7d2a8de509d07366c6f088caab63435e67ba49
Guan <Gyf482694326@outlook.com>
2024-05-12 00:16:02 +0800

    完成：打开数据库，连接tableview

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,27 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QSqlDatabase>
+#include <QMessageBox>
+#include <QSqlQueryModel>
+
+void MainWindow::openTable()
+{
+    queryModel = new QSqlQueryModel(this);
+    selectionModel = new QItemSelectionModel(queryModel, this);
+    ui->tableView_show->setModel(queryModel);
+    ui->tableView_show->setSelectionModel(selectionModel);
+
+    //查询语句
+    queryModel->setQuery("SELECT NAME, DATE_BEGIN, DATE_END FROM schedule;");
+
+    //设置列宽
+    int tableViewWidth = ui->tableView_show->width();
+    //ui->tableView_show->horizontalHeader()->setMinimumSectionSize();
+    ui->tableView_show->setColumnWidth(0, tableViewWidth / 3);
+    ui->tableView_show->setColumnWidth(1, tableViewWidth / 3);
+    ui->tableView_show->setColumnWidth(2, tableViewWidth / 3);
+}
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -12,3 +34,23 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_action_open_new_database_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, "选择一个数据库文件",
+                                                    databasePath, "sqlite数据库(*.db)");
+    if (fileName.isEmpty()) return;
+
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName(fileName);
+    db.open();
+
+    if (!db.isOpen()) {
+        QMessageBox::critical(this, "错误", "数据库文件打开失败，请重试！");
+    }
+    else {
+        openTable();
+    }
+
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QSqlDatabase>
+
+class QSqlQueryModel;
+class QItemSelectionModel;
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -13,9 +17,21 @@ class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
+private:
+    QString databasePath = "D:\\project\\QT\\TimeManagement"; //数据库路径
+    QSqlDatabase db;
+    QSqlQueryModel *queryModel;
+    QItemSelectionModel *selectionModel;
+
+private:
+    void openTable(); //打开表格
+
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void on_action_open_new_database_triggered();
 
 private:
     Ui::MainWindow *ui;
